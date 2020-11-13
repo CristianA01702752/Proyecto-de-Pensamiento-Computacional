e9f6e3052ad448a20febeacbd301ae39d09c3339
CristianA01702752 <74316936+CristianA01702752@users.noreply.github.com>
2020-11-12 20:47:41 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,49 @@
 using namespace std;
 #include "caracteristicas.h";
 
+#include <iostream>
+using namespace std;
+#include "caracteristicas.h"
+
+void menu (){
+  cout<<"1. Malphite"<<endl;
+  cout<<"2. Salir"<<endl;
+  
+}
+
 int main(){
     cout<<"Ingresa el nombre del campeón:"<<endl;
     cout<<"Malphite"<<endl;
 	
-	itemizacion a1("Botas de Mercurio", "Corazón de Hielo", "Hidra Titánica");
-	cout<<"Las botas más utilizadas con Malphite son: "<<a1.get_botas()<<endl;
-	cout<<"El objeto defensivo más usado con Malphite es: "<<a1.get_defensivos()<<endl;
-	cout<<"El objeto ofensivo más usado con Malphite es: "<<a1.get_ofensivos()<<endl;
-	
-	counterPicks a2("Garen e Illaoi", "Shen y Mordekaiser");
-	cout<<"Malphite es débil al enfrentarse contra: "<<a2.get_enemigosF()<<endl;
-	cout<<"Malphite es fuerte al enfrentarse contra: "<<a2.get_enemigosD()<<endl;
-}
+	  itemizacion malphite= itemizacion("Botas de Mercurio", "Corazón de Hielo", "Hidra Titánica");
+
+		counterPicks malphite1= counterPicks("Garen e Illaoi", "Shen y Mordekaiser");
+
+    runas malphite2= runas("Cometa arcano, Banda de Maná, Trascendencia y Pirolasér","Entrega de galletas y Tónico de distorsión temporal", "2 puntos ofensivos y 1 punto en armadura");
+    
+
+    
+    bool ciclo = true;
+    while (ciclo){
+      menu();
+        int eleccion;
+        std::cout << "Elige a tu campeon: ";
+        std::cin >> eleccion;
+    
+        if (eleccion==1){
+          malphite.set_botas();
+          malphite.set_defensivos();
+          malphite.set_ofensivos();
+          malphite1.set_enemigosF();
+          malphite1.set_enemigosD();
+          malphite2.set_runasp();
+          malphite2.set_runass();
+          malphite2.set_runast();
+        }
+        else if (eleccion==2){
+          cout<<"Adiós, invocador"<<endl;
+          exit(-1);
+      }
+    }
+  }
+
