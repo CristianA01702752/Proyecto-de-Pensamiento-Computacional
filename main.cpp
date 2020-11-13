bf4cbafc2c174760a8728b95d8df32dff6f85995
CristianA01702752 <74316936+CristianA01702752@users.noreply.github.com>
2020-11-12 22:41:25 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,23 +2,20 @@
 //Cristian Rogelio Espinosa Díaz 
 //A01702752
 
+//se manda a llamar las bibliotecas y el programa que contienen las clases para que funcionen
 #include <iostream>
 using namespace std;
 #include "caracteristicas.h";
 
-#include <iostream>
-using namespace std;
-#include "caracteristicas.h"
-
+//se crea el menú para que el usuario eliga al personaje (por el momento solo hay un personaje ya que no podía hacer que funcionara)
 void menu (){
   cout<<"1. Malphite"<<endl;
   cout<<"2. Salir"<<endl;
   
 }
 
+//se crea la función main en donde se guardaran los datos que se imprimiran dependiendo del campeon que el usuario escoga
 int main(){
-    cout<<"Ingresa el nombre del campeón:"<<endl;
-    cout<<"Malphite"<<endl;
 	
     itemizacion malphite= itemizacion("Botas de Mercurio", "Corazón de Hielo", "Hidra Titánica");
 
@@ -26,8 +23,7 @@ int main(){
 
     runas malphite2= runas("Cometa arcano, Banda de Maná, Trascendencia y Pirolasér","Entrega de galletas y Tónico de distorsión temporal", "2 puntos ofensivos y 1 punto en armadura");
     
-
-    
+//se crea un ciclo while para que el usuario pueda checar los distintos aspectos de los campeones hasta que el decida terminar el programa con la opción salir.
     bool ciclo = true;
     while (ciclo){
       menu();
