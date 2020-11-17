070b8e4ac4411b55d20308fc05f978b4264e489e
CristianA01702752 <74316936+CristianA01702752@users.noreply.github.com>
2020-11-16 20:05:58 -0600

    Update caracteristicas.h

--- caracteristicas.h
+++ caracteristicas.h
@@ -5,8 +5,6 @@
 #include <iostream>
 using namespace std;
 
-//se define la clase para que el programa organice y pueda desplegar en la pantalla la información de los "items" de los campeones
-
 class itemizacion{
 	private:
 		string botas;
@@ -14,31 +12,41 @@ class itemizacion{
 		string ofensivos;
 		
 	public:
-		itemizacion(string, string, string);
+		itemizacion(string bot, string def, string ofe): botas(bot), defensivos(def), ofensivos(ofe){};
 		
-		void set_botas();
-		void set_defensivos();
-		void set_ofensivos();
+		void set_botas(string);
+		void set_defensivos(string);
+		void set_ofensivos(string);
+
+    string get_botas();
+    string get_defensivos();
+    string get_ofensivos();
 	
 };
 
-// se crea un constructor para que los datos que se asignen a las partes correspondientes que se asignaran a cada campeón
-itemizacion:: itemizacion(string bot, string def, string ofe): botas(bot), defensivos(def), ofensivos(ofe){};
-
-// se crea la instrucción para que el programa imprima el mensaje con los datos correspondientes
-void itemizacion::set_botas(){
-  cout<<"Las botas más utilizadas con Malphite son: "<<botas<<endl;
-}
-// se crea la instrucción para que el programa imprima el mensaje con los datos correspondientes
-void itemizacion::set_defensivos(){
-  cout<<"El objeto defensivo más usado con Malphite es: "<<defensivos<<endl;
-}
-// se crea la instrucción para que el programa imprima el mensaje con los datos correspondientes
-void itemizacion::set_ofensivos(){
-  cout<<"El objeto ofensivo más usado con Malphite es: "<<ofensivos<<endl;
+string itemizacion::get_botas(){
+  return botas;
 }
 
-//se define la clase para que el programa organice y pueda desplegar en la pantalla la información de los "items" de los campeones
+string itemizacion::get_defensivos(){
+  return defensivos;
+}
+
+string itemizacion::get_ofensivos(){
+  return ofensivos;
+}
+void itemizacion::set_botas(string bot){
+  botas=bot;
+}
+
+void itemizacion::set_defensivos(string def){
+  defensivos=def;
+}
+
+void itemizacion::set_ofensivos(string ofe){
+  ofensivos=ofe;
+}
+
 
 class counterPicks{
 	private:
@@ -46,25 +54,32 @@ class counterPicks{
 		string enemigosD;
 		
 	public:
-		counterPicks(string, string);
+		counterPicks(string fuertes, string debiles): enemigosF(fuertes), enemigosD(debiles){};
 		
-		void set_enemigosF();
-		void set_enemigosD();
+		void set_enemigosF(string);
+		void set_enemigosD(string);
+
+    string get_enemigosF();
+    string get_enemigosD();
 	
 };
-// se crea un constructor para que los datos que se asignen a las partes correspondientes que se asignaran a cada campeon
-counterPicks:: counterPicks(string fuertes, string debiles): enemigosF(fuertes), enemigosD(debiles){};
 
-// se crea la instrucción para que el programa imprima el mensaje con los datos correspondientes
-void counterPicks::set_enemigosF(){
-	cout<<"Malphite es débil al enfrentarse contra: "<<enemigosF<<endl;	
-}
-// se crea la instrucción para que el programa imprima el mensaje con los datos correspondientes
-void counterPicks::set_enemigosD(){
-  cout<<"Malphite es fuerte al enfrentarse contra: "<<enemigosD<<endl;
+string counterPicks::get_enemigosF(){
+  return enemigosF;
 }
 
-//se define la clase para que el programa organice y pueda desplegar en la pantalla la información de los "items" de los campeones
+string counterPicks::get_enemigosD(){
+  return enemigosD;
+}
+
+void counterPicks::set_enemigosF(string fuertes){
+	enemigosF=fuertes;	
+}
+
+void counterPicks::set_enemigosD(string debiles){
+  enemigosD=debiles;
+}
+
 class runas{
 	private:
 		string runas_principales;
@@ -72,28 +87,38 @@ class runas{
 		string runas_terciarias;
 		
 	public:
-		runas(string, string, string);
+		runas(string rp, string rs, string rt): runas_principales(rp), runas_secundarias(rs), runas_terciarias(rt){};
 		
-		void set_runasp();
-		void set_runass();
-		void set_runast();
+		void set_runasp(string);
+		void set_runass(string);
+		void set_runast(string);
+
+    string get_runasp();
+    string get_runass();
+    string get_runast();
 	
 };
-// se crea un constructor para que los datos que se asignen a las partes correspondientes que se asignaran a cada campeon
-runas:: runas(string rp, string rs, string rt): runas_principales(rp), runas_secundarias(rs), runas_terciarias(rt){};
 
-// se crea la instrucción para que el programa imprima el mensaje con los datos correspondientes
-void runas::set_runasp(){
-  cout<<"Las runas principales son:"<<endl;
-  cout<<runas_principales<<endl;
+string runas:: get_runasp(){
+  return runas_principales;
 }
-// se crea la instrucción para que el programa imprima el mensaje con los datos correspondientes
-void runas::set_runass(){
-  cout<<"Las runas secundarias son: "<<endl;
-  cout<<runas_secundarias<<endl;
+
+string runas:: get_runass(){
+  return runas_secundarias;
 }
-// se crea la instrucción para que el programa imprima el mensaje con los datos correspondientes
-void runas::set_runast(){
-  cout<<"El objeto ofensivo más usado con Malphite es: "<<endl;
-  cout<<runas_terciarias<<endl;
+
+string runas:: get_runast(){
+  return runas_terciarias;
+}
+
+void runas::set_runasp(string rp){
+  runas_principales=rp;
+}
+
+void runas::set_runass(string rs){
+  runas_secundarias=rs;
+}
+
+void runas::set_runast(string rt){
+  runas_terciarias=rt;
 }
