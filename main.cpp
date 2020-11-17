5f7cd96e978f0312ea406a3f937f537982e91720
CristianA01702752 <74316936+CristianA01702752@users.noreply.github.com>
2020-11-16 20:04:39 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,57 +2,66 @@
 //Cristian Rogelio Espinosa Díaz 
 //A01702752
 
-//se manda a llamar las bibliotecas y el programa que contienen las clases para que funcionen
 #include <iostream>
 using namespace std;
-#include "caracteristicas.h";
+#include "caracteristicas.h"
 
-//se crea el menú para que el usuario eliga al personaje (por el momento solo hay un personaje ya que no podía hacer que funcionara)
 void menu (){
   cout<<"1. Malphite"<<endl;
   cout<<"2. Salir"<<endl;
   
 }
 
-//se crea la función main en donde se guardaran los datos que se imprimiran dependiendo del campeon que el usuario escoga
 int main(){
-	
-    itemizacion malphite= itemizacion("Botas de Mercurio", "Corazón de Hielo", "Hidra Titánica");
+  
+itemizacion malphite= itemizacion("Botas de Mercurio", "Corazón de Hielo", "Hidra Titánica");
 
-    counterPicks malphite1= counterPicks("Garen e Illaoi", "Shen y Mordekaiser");
+counterPicks malphite1= counterPicks("Garen e Illaoi", "Shen y Mordekaiser");
 
-    runas malphite2= runas("Cometa arcano, Banda de Maná, Trascendencia y Pirolasér","Entrega de galletas y Tónico de distorsión temporal", "2 puntos ofensivos y 1 punto en armadura");
+runas malphite2= runas("Cometa arcano, Banda de Maná, Trascendencia y Pirolasér","Entrega de galletas y Tónico de distorsión temporal", "2 puntos ofensivos y 1 punto en armadura");
     
-//se crea un ciclo while para que el usuario pueda checar los distintos aspectos de los campeones hasta que el decida terminar el programa con la opción salir.
+
+    
     bool ciclo = true;
     while (ciclo){
       menu();
-        int eleccion;
-        std::cout << "Elige a tu campeon: ";
+        string eleccion;
+        std::cout << "Ingresa el nombre de tu campeon: ";
         std::cin >> eleccion;
     
-        if (eleccion==1){
-          malphite.set_botas();
+        if (eleccion=="Maplhite"||eleccion=="malphite"){
           cout<<""<<endl;
-          malphite.set_defensivos();
+          
+          cout<<"Las botas más utilizadas con Malphite son: "<<malphite.get_botas()<<endl;
           cout<<""<<endl;
-          malphite.set_ofensivos();
+
+          cout<<"El objeto defensivo más usado con Malphite es: "<<malphite.get_defensivos()<<endl;
           cout<<""<<endl;
-          malphite1.set_enemigosF();
+          
+          cout<<"El objeto ofensivo más usado con Malphite es: "<<malphite.get_ofensivos()<<endl;
           cout<<""<<endl;
-          malphite1.set_enemigosD();
+          
+          cout<<"Malphite es débil al enfrentarse contra: "<<malphite1.get_enemigosF()<<endl;
           cout<<""<<endl;
-          malphite2.set_runasp();
+          
+          cout<<"Malphite es fuerte al enfrentarse contra: "<<malphite1.get_enemigosD()<<endl;
           cout<<""<<endl;
-          malphite2.set_runass();
+          
+          cout<<"Las runas principales son:"<<endl;
+          cout<<malphite2.get_runasp()<<endl;
           cout<<""<<endl;
-          malphite2.set_runast();
+          
+          cout<<"Las runas secundarias son: "<<endl;
+          cout<<malphite2.get_runass()<<endl;
+          cout<<""<<endl;
+          
+          cout<<"Las runas terciarias son: "<<endl;
+          cout<<malphite2.get_runast()<<endl;
           cout<<""<<endl;
         }
-        else if (eleccion==2){
+        else if (eleccion=="Adios"||eleccion=="adios"){
           cout<<"Adiós, invocador"<<endl;
           exit(-1);
       }
     }
   }
-
