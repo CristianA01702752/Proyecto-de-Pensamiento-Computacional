e698a3f5e133c88d189401b20190f9c59d855247
CristianA01702752 <74316936+CristianA01702752@users.noreply.github.com>
2020-11-27 08:43:34 -0600

    Update caracteristicas.h

--- caracteristicas.h
+++ caracteristicas.h
@@ -2,6 +2,9 @@
 //Cristian Rogelio Espinosa Díaz 
 //A01702752
 
+#ifndef CARACTERISTICAS_H_
+#define CARACTERISTICAS_H_
+#include <sstream>
 #include <iostream>
 using namespace std;
 
@@ -12,6 +15,7 @@ class itemizacion{
 		string ofensivos;
 		
 	public:
+    itemizacion(){};
 		itemizacion(string bot, string def, string ofe): botas(bot), defensivos(def), ofensivos(ofe){};
 		
 		void items_botas(string);
@@ -21,8 +25,10 @@ class itemizacion{
     		string get_botas();
     		string get_defensivos();
     		string get_ofensivos();
+        string to_string()const;
 	
 };
+
 
 string itemizacion::get_botas(){
   return botas;
@@ -47,6 +53,11 @@ void itemizacion::items_ofensivos(string ofe){
   ofensivos=ofe;
 }
 
+string itemizacion:: to_string()const{
+    stringstream aux;
+    aux<<" "<<botas<<", "<<defensivos<<", "<<ofensivos;
+    return aux.str();
+}
 
 class counterPicks{
 	private:
@@ -54,6 +65,7 @@ class counterPicks{
 		string enemigosD;
 		
 	public:
+    counterPicks(){};
 		counterPicks(string fuertes, string debiles): enemigosF(fuertes), enemigosD(debiles){};
 		
 		void s_enemigosF(string);
@@ -61,6 +73,7 @@ class counterPicks{
 
     		string get_enemigosF();
     		string get_enemigosD();
+        string to_string1()const;
 	
 };
 
@@ -78,6 +91,12 @@ void counterPicks::s_enemigosF(string fuertes){
 
 void counterPicks::s_enemigosD(string debiles){
   enemigosD=debiles;
+}
+
+string counterPicks:: to_string1()const{
+    stringstream aux;
+    aux<<" "<<enemigosF<<" y tu personaje es fuerte contra: "<< enemigosD <<endl;
+    return aux.str();
 }
 
 class runas_p{
@@ -162,6 +181,7 @@ class runas_s: public runas_p {
     void s_runas2(string);
     string get_runas1();
     string get_runas2();
+    string to_string2()const;
     
 };
 
@@ -196,3 +216,9 @@ string runas_s:: get_runas2(){
   return runas_2;
 }
 
+string runas_s:: to_string2()const{
+    stringstream aux;
+    aux<<runa_1<<", "<<runa_2<<", "<<runa_3<<" y "<<runa_4<<"y las runas secundarias son: "<<runas_1<<" y "<<runas_2 <<endl;
+    return aux.str();
+}
+#endif 
