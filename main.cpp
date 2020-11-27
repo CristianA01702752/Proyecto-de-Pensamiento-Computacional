c859ba29cd456f11a087842bfb1a3164e5538b2e
CristianA01702752 <74316936+CristianA01702752@users.noreply.github.com>
2020-11-27 08:42:18 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,174 +4,64 @@
 
 #include <iostream>
 using namespace std;
-#include "caracteristicas.h"
+#include "toplane.h"
 
 void menu (){
   cout<<"1. Malphite"<<endl;
-  cout<<"2.Volibear"<<endl;
-  cout<<"3.Mordekaiser"<<endl;
+  cout<<"2. Volibear"<<endl;
+  cout<<"3. Mordekaiser"<<endl;
   cout<<"4. Salir"<<endl;
   
 }
 
 int main(){
-
-	itemizacion malphite= itemizacion("Botas de Mercurio", "Corazón de Hielo", "Hidra Titánica");
-
-	counterPicks malphite1= counterPicks("Garen e Illaoi", "Shen y Mordekaiser");
-
-    runas_s malphite3= runas_s("Cometa arcano", "Banda de Maná", "Trascendencia", "Pirolasér","Entrega de galleta","Tónico de distorsión temporal");
-    
-    itemizacion volibear= itemizacion("Botas blindadas", "Égida de fuego solar", "Placa del hombre muerto");
-
-    counterPicks volibear1= counterPicks("Viktor, Kayle y Heirmendinger","Tryndamare, Rumble y Warwick");
-
-    runas_s volibear3= runas_s("Ataque intensificado","Triunfo","Tenacidad","Último esfuerzo","Calzado mágico","Perspicacia cósmica");
-
-    itemizacion mordekaiser= itemizacion("Botas blindadas","Agrietador","Apariencia fantasmal");
-
-    counterPicks mordekaiser1= counterPicks("Viktor, Kayle y Heirmendinger","Illaoi, Kalista y Tryndamere");
-
-    runas_s mordekaiser3= runas_s("Conquistador","Triunfo","Tenacidad","Último esfuerzo","Sabor a sangre","Cazador voraz");
-
-    
-    bool ciclo = true;
+  
+  bool ciclo = true;
     while (ciclo){
       menu();
         string eleccion;
         std::cout << "Ingresa el nombre de tu campeon: ";
         std::cin >> eleccion;
-    
+
         if (eleccion=="Maplhite"||eleccion=="malphite"){
           cout<<""<<endl;
-          
-          cout<<"Las botas más utilizadas con Malphite son: "<<malphite.get_botas()<<endl;
+          toplane malphite;
+          malphite.agregaitems(1,"Botas de Mercurio", "Corazón de Hielo", "Hidra Titánica");
+          malphite.agregapicks(1,"Garen e Illaoi", "Shen y Mordekaiser");
+          malphite.agregarunas(1,"Cometa arcano", "Banda de Maná", "Trascendencia", "Pirolasér","Entrega de galleta","Tónico de distorsión temporal");
+          malphite.muestra_items();
           cout<<""<<endl;
-
-          cout<<"El objeto defensivo más usado con Malphite es: "<<malphite.get_defensivos()<<endl;
+          malphite.muestra_picks();
           cout<<""<<endl;
-          
-          cout<<"El objeto ofensivo más usado con Malphite es: "<<malphite.get_ofensivos()<<endl;
-          cout<<""<<endl;
-          
-          cout<<"Malphite es débil al enfrentarse contra: "<<malphite1.get_enemigosF()<<endl;
-          cout<<""<<endl;
-          
-          cout<<"Malphite es fuerte al enfrentarse contra: "<<malphite1.get_enemigosD()<<endl;
-          cout<<""<<endl;
-          
-          cout<<"Las runas principales son:"<<endl;
-          cout<<""<<endl;
-
-          cout<<malphite3.get_runa1()<<endl;
-          cout<<""<<endl;
-          
-          cout<<malphite3.get_runa2()<<endl;
-          cout<<""<<endl;
-          
-          cout<<malphite3.get_runa3()<<endl;
-          cout<<""<<endl;
-
-          cout<<malphite3.get_runa4()<<endl;
-          cout<<""<<endl;
-
-          cout<<"Las runas secundarias son:"<<endl;
-          cout<<""<<endl;
-
-          cout<<malphite3.get_runas1()<<endl;
-          cout<<""<<endl;
-
-          cout<<malphite3.get_runas2()<<endl;
-          cout<<""<<endl;
+          malphite.muestra_runas();
         }
         else if (eleccion=="Volibear"||eleccion=="volibear"){
           cout<<""<<endl;
-          
-          cout<<"Las botas más utilizadas con Volibear son: "<<volibear.get_botas()<<endl;
+          toplane volibear;
+          volibear.agregaitems(1,"Botas blindadas", "Égida de fuego solar", "Placa del hombre muerto");
+          volibear.agregapicks(1,"Viktor, Kayle y Heirmendinger","Tryndamare, Rumble y Warwick");
+          volibear.agregarunas(1,"Ataque intensificado","Triunfo","Tenacidad","Último esfuerzo","Calzado mágico","Perspicacia cósmica");
+          volibear.muestra_items();
           cout<<""<<endl;
-
-          cout<<"El objeto defensivo más usado con Volibear es: "<<volibear.get_defensivos()<<endl;
+          volibear.muestra_picks();
           cout<<""<<endl;
-          
-          cout<<"El objeto ofensivo más usado con Volibear es: "<<volibear.get_ofensivos()<<endl;
-          cout<<""<<endl;
-          
-          cout<<"Volibear es débil al enfrentarse contra: "<<volibear1.get_enemigosF()<<endl;
-          cout<<""<<endl;
-          
-          cout<<"Volibear es fuerte al enfrentarse contra: "<<volibear1.get_enemigosD()<<endl;
-          cout<<""<<endl;
-          
-          cout<<"Las runas principales son:"<<endl;
-          cout<<""<<endl;
-
-          cout<<volibear3.get_runa1()<<endl;
-          cout<<""<<endl;
-          
-          cout<<volibear3.get_runa2()<<endl;
-          cout<<""<<endl;
-          
-          cout<<volibear3.get_runa3()<<endl;
-          cout<<""<<endl;
-
-          cout<<volibear3.get_runa4()<<endl;
-          cout<<""<<endl;
-
-          cout<<"Las runas secundarias son:"<<endl;
-          cout<<""<<endl;
-
-          cout<<volibear3.get_runas1()<<endl;
-          cout<<""<<endl;
-
-          cout<<volibear3.get_runas2()<<endl;
-          cout<<""<<endl;
+          volibear.muestra_runas();
         }
         else if (eleccion=="Mordekaiser"||eleccion=="mordekaiser"){
           cout<<""<<endl;
-          
-          cout<<"Las botas más utilizadas con Mordekaiser son: "<<mordekaiser.get_botas()<<endl;
+          toplane mordekaiser;
+          mordekaiser.agregaitems(1,"Botas blindadas","Agrietador","Apariencia fantasmal");
+          mordekaiser.agregapicks(1,"Viktor, Kayle y Heirmendinger","Illaoi, Kalista y Tryndamere");
+          mordekaiser.agregarunas(1,"Conquistador","Triunfo","Tenacidad","Último esfuerzo","Sabor a sangre","Cazador voraz");
+          mordekaiser.muestra_items();
           cout<<""<<endl;
-
-          cout<<"El objeto defensivo más usado con Mordekaiser es: "<<mordekaiser.get_defensivos()<<endl;
+          mordekaiser.muestra_picks();
           cout<<""<<endl;
-          
-          cout<<"El objeto ofensivo más usado con Mordekaiser es: "<<mordekaiser.get_ofensivos()<<endl;
-          cout<<""<<endl;
-          
-          cout<<"Mordekaiser es débil al enfrentarse contra: "<<mordekaiser1.get_enemigosF()<<endl;
-          cout<<""<<endl;
-          
-          cout<<"Mordekaiser es fuerte al enfrentarse contra: "<<mordekaiser1.get_enemigosD()<<endl;
-          cout<<""<<endl;
-          
-          cout<<"Las runas principales son:"<<endl;
-          cout<<""<<endl;
-
-          cout<<mordekaiser3.get_runa1()<<endl;
-          cout<<""<<endl;
-          
-          cout<<mordekaiser3.get_runa2()<<endl;
-          cout<<""<<endl;
-          
-          cout<<mordekaiser3.get_runa3()<<endl;
-          cout<<""<<endl;
-
-          cout<<mordekaiser3.get_runa4()<<endl;
-          cout<<""<<endl;
-
-          cout<<"Las runas secundarias son:"<<endl;
-          cout<<""<<endl;
-
-          cout<<mordekaiser3.get_runas1()<<endl;
-          cout<<""<<endl;
-
-          cout<<mordekaiser3.get_runas2()<<endl;
-          cout<<""<<endl;
+          mordekaiser.muestra_runas();
         }
-        
-        
+
         else if (eleccion=="Salir"||eleccion=="salir"){
-          cout<<"Adiós, invocador"<<endl;
+          cout<<"Adiós invocador"<<endl;
           exit(-1);
         
         }
@@ -179,6 +69,6 @@ int main(){
           cout<<"Lo sentimos invocador pero ese personaje no existe o no ha sido registrado a la base de datos"<<endl;
           cout<<""<<endl;
           cout<<"Intenta otra vez"<<endl;
-      }
+        }
     }
-  }
+}
