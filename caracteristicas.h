f59b114e8e892f839a476ccf22c1906c23739510
CristianA01702752 <74316936+CristianA01702752@users.noreply.github.com>
2020-12-01 10:50:15 -0600

    Update caracteristicas.h

--- caracteristicas.h
+++ caracteristicas.h
@@ -1,23 +1,36 @@
-// Proyecto avance 1
-//Cristian Rogelio Espinosa Díaz 
-//A01702752
+/* Proyecto avance 1
+ * Cristian Rogelio Espinosa Díaz 
+ * A01702752
+ */
 
 #ifndef CARACTERISTICAS_H_
 #define CARACTERISTICAS_H_
 #include <sstream>
 #include <iostream>
 using namespace std;
+/*
+ * Este archivo contiene las 3 clases que se van a usar para que el programa funcione
+ * las cuales son itemizacion, counterPicks y runas_p.
+ * runas_p tendrá una clase hija la cual se llama runas_s, 
+ * cuyo propósito es heredar los objetos de la clase padre para poder 
+ * tener los 6 valores en uno solo
+ */
 
+// Declaración de la clase itemizacion
 class itemizacion{
 	private:
+	 // Declaro las variables de la clase
 	 string botas;
 	 string defensivos;
 	 string ofensivos;
 		
 	public:
+	  /* Declaro los métodos que se utilizarán en la clase
+	   * Declaro tanto un constructor default como un constructor con las variables 
+	   */ 
     	 itemizacion(){};
 	 itemizacion(string bot, string def, string ofe): botas(bot), defensivos(def), ofensivos(ofe){};
-		
+	
 	 void items_botas(string);
 	 void items_defensivos(string);
 	 void items_ofensivos(string);
@@ -28,26 +41,62 @@ class itemizacion{
          string to_string()const;
 };
 
-
+/*
+ * getter get_botas
+ *
+ * @param
+ * @return string: botas del personaje
+ */
 string itemizacion::get_botas(){
   return botas;
 }
 
+/*
+ * getter get_defensivos
+ *
+ * @param
+ * @return string: objeto defensivo del personaje
+ */
 string itemizacion::get_defensivos(){
   return defensivos;
 }
 
+/*
+ * getter get_ofensivos
+ *
+ * @param
+ * @return string: objeto ofensivo del personaje
+ */
 string itemizacion::get_ofensivos(){
   return ofensivos;
 }
+
+/*
+ * setter items_botas
+ *
+ * @param string bot
+ * @return variable botas
+ */
 void itemizacion::items_botas(string bot){
   botas=bot;
 }
 
+/*
+ * setter items_defensivos
+ *
+ * @param string def
+ * @return variable defensivos
+ */
 void itemizacion::items_defensivos(string def){
   defensivos=def;
 }
 
+/*
+ * setter items_ofensivos
+ *
+ * @param string ofe
+ * @return variable ofensivos
+ */
 void itemizacion::items_ofensivos(string ofe){
   ofensivos=ofe;
 }
